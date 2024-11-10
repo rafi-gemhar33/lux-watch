e8efa51fac75b7a8aecae57740b63947d3fb0cf5
MelbinTomy <melbintomy40@gmail.com>
2024-11-10 14:20:01 -0500

    About us, Contact us, Page Not found, Navbar and footer added.

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import Layout from '../components/Layout/Layout.js';
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+import contactUs from "../Images/contactUs.jpg";
 
 const Contact = () => {
     return (
         <Layout>
-            <h1>Contact Page</h1>
+            <div className="contact-container row">
+                <div className="col-md-6">
+                    <img src={contactUs} alt="Contact Us" className="contact-image" />
+                </div>
+                <div className="col-md-6">
+                    <h1 className="contact-title">Contact Us</h1>
+                    <p className="contact-description">
+                        Should you have any inquiries or require further information, please don't hesitate to contact us. Our team is available 24/7 to assist you.
+                    </p>
+                    <p className="contact-info">
+                        <BiMailSend /> : <a href="mailto:www.help@ecommerceapp.com">www.help@ecommerceapp.com</a>
+                    </p>
+                    <p className="contact-info">
+                        <BiPhoneCall /> : (107) 123-9909
+                    </p>
+                    <p className="contact-info">
+                        <BiSupport /> : 888-000-1111 (Support)
+                    </p>
+                </div>
+            </div>
         </Layout>
     );
 };
--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Layout from '../components/Layout/Layout.js';
+
 
 const Pagenotfound = () => {
     return (
         <Layout>
-            <h1>Page Not Found !</h1>
+            <div className="pnotfound">
+                <h1 className='pnotfound-title'>404</h1>
+                <h2 className='pnotfound-heading'>We couldn't find the page you were looking for.</h2>
+                <Link to="/" className="pnotfound-btn">
+                    Go Back
+                </Link>
+            </div>
         </Layout>
     );
 };
